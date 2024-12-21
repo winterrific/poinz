ae36c1f178fd836ce0e079836f370d4dda52a25c
Ruben Dorozala <ruben@winterrific.net>
2024-12-21 19:24:49 +0100

    feat: update eslint to version 9.x which needs to have new config files

---
+++ server/eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from 'globals';
+export default [
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node
+      },
+
+      ecmaVersion: 2020,
+      sourceType: 'module'
+    },
+
+    rules: {
+      semi: [2, 'always'],
+      eqeqeq: 2,
+      quotes: [2, 'single'],
+      curly: [2, 'all'],
+      'prefer-const': 2,
+      'no-var': 2,
+      'no-use-before-define': 0,
+      'no-console': 0
+    }
+  }
+];
